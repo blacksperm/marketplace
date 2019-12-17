b42155d0a0e78a6bdfaf5a9d2dc5a5e1e3887ac9
Rudy Portillo <rudyportillo21@gmail.com>
2019-12-16 21:30:20 -0600

    arreglo de codigo

--- application/helpers/ajax_propuesta_helper.php
+++ application/helpers/ajax_propuesta_helper.php
@@ -41,7 +41,7 @@
 		//eliminar Aqui me quede 
 
 
-				$('#tabla_propuesta').on('click','.borrar', function(){
+		$('#tabla_propuesta').on('click','.borrar', function(){
 			$id = $(this).attr('data');// para capturar el dato segun el dato que demos click
 			$('#modalBorrar').modal('show');//para mostrar el modal
 			$('#btnBorrar').unbind().click(function(){
@@ -70,74 +70,72 @@
 
 		});
 
-				$('#nuevaPro').click(function(){
-					$('#propuesta').modal('show');
-					$('#propuesta').find('.modal-title').text('Nueva Propuesta');
-					$('#formPropuesta').attr('action','<?= base_url('propuesta_controller/ingresar') ?>');
+		$('#nuevaPro').click(function(){
+			$('#propuesta').modal('show');
+			$('#propuesta').find('.modal-title').text('Nueva Propuesta');
+			$('#formPropuesta').attr('action','<?= base_url('propuesta_controller/ingresar') ?>');
 
 
-				});
+		});
 
-				get_estado();
-				function get_estado(){
-					$.ajax({
-						type: 'ajax',
-						url: '<?= base_url('propuesta_controller/get_estado') ?>',
-						dataType: 'json',
+		get_estado();
+		function get_estado(){
+			$.ajax({
+				type: 'ajax',
+				url: '<?= base_url('propuesta_controller/get_estado') ?>',
+				dataType: 'json',
 
-						success: function(datos){
-							var op = '';
-							var i;
-							op +="<option value=''>--Seleccione un Estado--</option>";
-							for(i=0; i<datos.length; i++){
-								op +="<option value='"+datos[i].id_estado+"'>"+datos[i].estado+"</option>";
-							}
-							$('#estado').html(op);
-						}
-					});
+				success: function(datos){
+					var op = '';
+					var i;
+					op +="<option value=''>--Seleccione un Estado--</option>";
+					for(i=0; i<datos.length; i++){
+						op +="<option value='"+datos[i].id_estado+"'>"+datos[i].estado+"</option>";
+					}
+					$('#estado').html(op);
 				}
+			});
+		}
 
 
 
-<<<<<<< HEAD
-					$.ajax({
+		<<<<<<< HEAD
+		$.ajax({
 
-						type: 'ajax',
-						method: 'post',
-						url: $url,
-						data: $data,
-						dataType: 'json',
-						success: function(respuesta){
-						$('#propuesta').modal('hide');
+			type: 'ajax',
+			method: 'post',
+			url: $url,
+			data: $data,
+			dataType: 'json',
+			success: function(respuesta){
+				$('#propuesta').modal('hide');
 
 
-						if(respuesta=='add'){
-							alertify.notify('Aplico al requerimiento exitosamente','success',10,null);
-						}
-						else if (respuesta=='edi'){
-							alertify.notify('Actualizado exitosamente!','success',10,null);
-						}
-						else{
-							alertify.notify('Error al Ingresar','error',10,null);
-						}
-						$('#formPropuesta')[0].reset();
-						mostrar_propuesta();
-						
+				if(respuesta=='add'){
+					alertify.notify('Aplico al requerimiento exitosamente','success',10,null);
 				}
-				});
-				});
-=======
->>>>>>> rigo
+				else if (respuesta=='edi'){
+					alertify.notify('Actualizado exitosamente!','success',10,null);
+				}
+				else{
+					alertify.notify('Error al Ingresar','error',10,null);
+				}
+				$('#formPropuesta')[0].reset();
+				mostrar_propuesta();
+				
+			}
+		});
 
 
-				$('#tabla_propuesta').on('click','.item-edit',function(){
-					var id= $(this).attr('data');
-					$('#propuesta').modal('show');
-					$('#propuesta').find('.modal-title').text('Actualizar Propuesta');
-					$('#formPropuesta').attr('action','<?= base_url('propuesta_controller/actualizar') ?>');
+
+		$('#tabla_propuesta').on('click','.item-edit',function(){
+			var id= $(this).attr('data');
+			$('#propuesta').modal('show');
+			$('#propuesta').find('.modal-title').text('Actualizar Propuesta');
+			$('#formPropuesta').attr('action','<?= base_url('propuesta_controller/actualizar') ?>');
 
 
-				$.ajax({
+			$.ajax({
 				type: 'ajax',
 				method: 'post',
 				url: '<?= base_url('propuesta_controller/get_datos') ?>',
@@ -154,8 +152,8 @@
 					$('#precio').val($datos.precio);
 				}
 
-				});
 			});
+		});
 
 
 
